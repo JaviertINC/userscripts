cdfdcdf5a24b12c2a28dc0ad722f8930e11fc971
@JaviertINC <luis.cortes.jaar@gmail.com>
2020-03-12 14:41:38 -0300

    Update github.darkgreen.user.js

--- scripts/github.darkgreen.user.js
+++ scripts/github.darkgreen.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          GitHub | Dark Green
-// @version       3.3
+// @version       3.4
 // @description	  Github Darker Green
 // @icon          https://www.google.com/s2/favicons?domain=github.com
 // @author        @JaviertINC
@@ -116,6 +116,8 @@
 		".ajax-pagination-form .ajax-pagination-btn:hover{ text-decoration: underline!important; }",
 		".dashboard-notice { background-color: #4c4c4c!important; border: 1px solid #4caf50!important; }",
 		".tabnav-tab.selected, .tabnav-tab[aria-current], .tabnav-tab[aria-selected=\"true\"] { background-color: #242424!important; border-color: #4caf50!important; }",
+		".dashboard-notice h2{color: #f1f1f1!important;}",
+		".dashboard-notice h2 strong,.dashboard-notice ul li strong{color: #4caf50!important;}",
 
 
 
