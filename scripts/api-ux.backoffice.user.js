fd68721c25c5b95d73a655b71dc67f2b8f9d871a
@JaviertINC <luis.cortes.jaar@gmail.com>
2020-03-13 10:35:36 -0300

    Update api-ux.backoffice.user.js

--- scripts/api-ux.backoffice.user.js
+++ scripts/api-ux.backoffice.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         API-UX | Better BackOffice
-// @version      2.4
+// @version      2.7
 // @description  Mejoramiento para el registro de horas en el BackOffice de Apiux
 // @icon         https://www.google.com/s2/favicons?domain=www.api-ux.com
 // @author       @JaviertINC
@@ -12,34 +12,45 @@
 // ==/UserScript==
 
 (function() {
-    	'use strict';
+    'use strict';
 
-    	var url_profile_img = ""; //Coloca aquí el enlace a tu imágen de perfil que quieres ver en el BackOffice de API-UX
+    var url_profile_img = ""; //Coloca aquí el enlace a tu imágen de perfil que quieres ver en el BackOffice de API-UX
+
+	/*
+	 *
+	 *     Borraaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaar
+	 *
+	 */
+	url_profile_img = "https://avatars3.githubusercontent.com/u/25622605";
 
 
-    	var sheet = document.styleSheets[0];
+    var sheet = document.styleSheets[0];
 
-    	//Top Anouncament Bar (Hidden)
-    	_s(sheet, "#announcement_bar_table", "display: none;");
+    //Top Anouncament Bar (Hidden)
+    _s(sheet, "#announcement_bar_table", "display: none;");
 
-    	_s(sheet, "#oe_main_menu_navbar", "background-color: #444; color: #4caf50; border: none; border-bottom: 1px solid #4caf50;");
-    	_s(sheet, "#oe_main_menu_navbar li a, #oe_main_menu_navbar li button", "padding: 6px 8px 6px 8px!important; margin: 0px!important;");
-    	_s(sheet, ".navbar-inverse .navbar-nav > .active > a, .navbar-inverse .navbar-nav > .active > a:hover, .navbar-inverse .navbar-nav > .active > a:focus", "background-color: #3d3d3d!important;");
+    _s(sheet, "#oe_main_menu_navbar", "background-color: #444; color: #4caf50; border: none; border-bottom: 1px solid #4caf50;");
+    _s(sheet, "#oe_main_menu_navbar li a, #oe_main_menu_navbar li button", "padding: 6px 8px 6px 8px!important; margin: 0px!important;");
+    _s(sheet, ".navbar-inverse .navbar-nav > .active > a, .navbar-inverse .navbar-nav > .active > a:hover, .navbar-inverse .navbar-nav > .active > a:focus", "background-color: #3d3d3d!important;");
 
-    	_s(sheet, "[data-id=\"name\"]", "max-width: 300px!important;");
-    	_s(sheet, "[data-field=\"name\"]", "max-width: 300px!important;");
-    	_s(sheet, "[data-field=\"name\"]", "white-space: nowrap!important; overflow: hidden!important; display: inline-block!important; text-overflow: ellipsis!important;");
+    _s(sheet, "[data-id=\"name\"]", "max-width: 300px!important;");
+    _s(sheet, "[data-field=\"name\"]", "max-width: 300px!important;");
+    _s(sheet, "[data-field=\"name\"]", "white-space: nowrap!important; overflow: hidden!important; display: inline-block!important; text-overflow: ellipsis!important;");
 
-    	_s(sheet, ".openerp .oe_list_content > tbody > tr > td.oe_list_field_cell", "white-space: pre-line;");
-    	_s(sheet, ".openerp .oe_tag_dark", "background-color: #4caf50!important; color: #eee!imporant;");
-    	_s(sheet, ".openerp .oe_loading", "font-size: 16px!important; font-weight: bold!important; top: 3%!important; padding: 6px 16px; background-color: #222!important; color: white; border: 1px solid #4caf50!important; border-radius: 8px!important;");
-    	_s(sheet, ".openerp button.oe_highlight", "background-color: #4caf50!important; background-image: unset!important; -moz-box-shadow: unset!important; -webkit-box-shadow: unset!important; box-shadow: unset!important;");
-    	_s(sheet, ".openerp .oe_application a", "color: #4caf50!important; ");
- 	_s(sheet, ".openerp .oe_searchview .oe_searchview_facets .oe_searchview_facet .oe_facet_remove:hover", "background-color: #4caf50!important; color: #ffffff!important; ");
+    _s(sheet, "[data-id=\"account_id\"]", "max-width: 284px!important;");
+    _s(sheet, "[data-field=\"account_id\"]", "max-width: 284px!important;");
+    _s(sheet, "[data-field=\"account_id\"]", "white-space: nowrap!important; overflow: hidden!important; text-overflow: ellipsis!important;");
+
+	_s(sheet, ".openerp .oe_list_content > tbody > tr > td.oe_list_field_cell", "white-space: pre-line;");
+	_s(sheet, ".openerp .oe_tag_dark", "background-color: #4caf50!important; color: #eee!imporant;");
+	_s(sheet, ".openerp .oe_loading", "font-size: 16px!important; font-weight: bold!important; top: 3%!important; padding: 6px 16px; background-color: #222!important; color: white; border: 1px solid #4caf50!important; border-radius: 8px!important;");
+	_s(sheet, ".openerp button.oe_highlight", "background-color: #4caf50!important; background-image: unset!important; -moz-box-shadow: unset!important; -webkit-box-shadow: unset!important; box-shadow: unset!important;");
+	_s(sheet, ".openerp .oe_application a", "color: #4caf50!important; ");
+	_s(sheet, ".openerp .oe_searchview .oe_searchview_facets .oe_searchview_facet .oe_facet_remove:hover", "background-color: #4caf50!important; color: #ffffff!important; ");
  	_s(sheet, ".openerp .oe_searchview .oe_searchview_facets .oe_searchview_facet .oe_facet_remove", "color: #4caf50!important; ");
 	_s(sheet, ".openerp .oe_searchview.oe_focused", "border-color: #4caf50!important; -moz-box-shadow: 0 1px 2px #4caf50 inset!important; -webkit-box-shadow: 0 1px 2px #4caf50 inset!important; box-shadow: 0 1px 2px #4caf50 inset!important;" );
 	_s(sheet, "table, th, td","border: none!important;");
-	_s(sheet, ".openerp .oe_secondary_menu_section","color: #4caf50!important;");
+	_s(sheet, ".openerp .oe_secondary_menu_section,.openerp .oe_searchview_drawer dt","color: #4caf50!important;");
 	_s(sheet, ".openerp .nav-pills > li.active > a, .openerp a.list-group-item.active > a","background-color: #4caf50!important; color: white!important;");
 	_s(sheet, ".openerp .oe_tag", "border: 1px solid #4caf50!important;");
 	_s(sheet, ".openerp .oe_searchview .oe_searchview_facets .oe_searchview_facet .oe_facet_value", "border-left: 1px solid #4caf50!important; text-shadow: none!important; color: #4caf50!important; font-weight: bold!important;");
@@ -65,6 +76,21 @@
 	_s(sheet, ".openerp .oe_list_content > thead","border-bottom: 2px solid #4caf50!important;");
 	_s(sheet, ".openerp .oe_list_content > tbody > tr","border-top: 1px solid #4caf50!important;");
 	_s(sheet, ".openerp .oe_list_content > tfoot", "border-top: unset!important; border-bottom: unset!important; background: transparent!important;");
+	_s(sheet, ".openerp .oe_searchview_drawer .oe_searchview_section li", "background-color: #4c4c4c!important; border-radius: 5px!important; border: 1px solid #4caf50!important; padding: 0 3px!important;");
+	_s(sheet, ".openerp .oe_searchview_drawer","background-color: #333333!important;");
+	_s(sheet, ".oe_view_manager_wrapper","background-color: #343434!important;");
+	_s(sheet, ".openerp .oe_searchview_drawer dl dd ul li.badge"," background-color: #4caf50!important; border: 1px solid #fff!important; border-radius: 5px!important; padding: 5px 3px!important; font-size: 13px!important;");
+	_s(sheet, ".openerp .oe_mail .oe_msg","background-color: transparent!important;");
+	_s(sheet, ".openerp .oe_mail .oe_msg.oe_msg_composer_compact .oe_compact_inbox", "border: 1px solid #4caf50!important; background: #5f5f5f!important;");
+	_s(sheet, ".oe_suggest_title", "margin-top: -12px!important; ");
+	_s(sheet, ".openerp .oe_mail_wall_aside .oe_sidebar_suggestion", "background-color: #4c4c4c!important; padding-top: unset!important; border: 1px solid #4caf50!important; margin-bottom: 8px!important; padding-bottom: 8px!important; ");
+	_s(sheet, ".openerp .oe_mail_wall .oe_mail_wall_aside","width: 310px!important; ");
+	_s(sheet, ".openerp .oe_mail_wall .oe_mail","background:#4c4c4c!important; border: 1px solid #4caf50!important; border-radius: 5px!important; padding: 8px 5px;!important; width: 700px!important; margin-right: 0px!important; ");
+	_s(sheet, ".openerp .oe_view_manager_current > .oe_view_manager_header","box-shadow: unset!important; ");
+	_s(sheet, ".oe_header_row.oe_header_row_top td","padding: 0 20px!important; ");
+	_s(sheet, ".openerp .oe_mail .oe_msg","border: none!important; border-radius: 0!important; border-bottom: 1px solid #4caf50!important; ");
+	_s(sheet, ".openerp .oe_mail .oe_attach_label,.openerp .oe_mail button.oe_attach, .openerp .oe_mail button.oe_full","color: #4caf50!important; ");
+	_s(sheet, ".openerp a.button:link, .openerp a.button:visited, .openerp button, .openerp .oe_button, .openerp input[type=\"submit\"]{ border: 1px solid #4caf50!important; color: #f1f1f1!important; background-color: #4c4c4c!important; background-image: unset!important; -moz-box-shadow: unset!important; -webkit-box-shadow: unset!important; box-shadow: unset!important; text-shadow: unset!important; ");
 
 	var url_logo = atob("aHR0cDovL3d3dy5hcGktdXguY29tL3dwLWNvbnRlbnQvdXBsb2Fkcy8yMDE4LzEwL2xvZ28tYmxhbmNvLTIucG5n");
 	var url_favicon = atob("aHR0cHM6Ly9qYXZpZXJ0aW5jLmdpdGh1Yi5pby9saWJzL2ljb25zL29kb28uaWNv");
@@ -97,14 +123,14 @@
 		else if("addRule" in sheet) {
 	    		sheet.addRule(selector, rules, index);
 		}
-    	}
-    	function re_favicon(url_favicon){
+    }
+    function re_favicon(url_favicon){
 		var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
 		link.type = 'image/x-icon';
 		link.rel = 'shortcut icon';
 		link.href = url_favicon;
 		document.getElementsByTagName('head')[0].appendChild(link);
-    	}
+    }
 
 	document.addEventListener("DOMContentLoaded", function(){
 		setTimeout(() => {
