606ccd3a8660e077c063640c9cb1fc89510aef44
@JaviertINC <luis.cortes.jaar@gmail.com>
2020-03-13 15:27:14 -0300

    Update whatsapp.darkdefault.user.js

--- scripts/whatsapp.darkdefault.user.js
+++ scripts/whatsapp.darkdefault.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Whatsapp Web | Dark Mode
 // @version      0.1
-// @description  Activa la version Dark de Whatsapp Web
+// @description  Activa la version Dark de Whatsapp Web  (Modo oscuro de whatsapp por defecto)
 // @icon         https://www.google.com/s2/favicons?domain=whatsapp.com
 // @author       @JaviertINC
 // @match        *://web.whatsapp.com/*
