88954de9476e8fde172fa7603162052d27bed2b4
@JaviertINC <luis.cortes.jaar@gmail.com>
2020-03-13 15:24:25 -0300

    Create whatsapp.darkdefault.user.js

---
+++ scripts/whatsapp.darkdefault.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Whatsapp Web | Dark Mode
+// @version      0.1
+// @description  Activa la version Dark de Whatsapp Web
+// @icon         https://www.google.com/s2/favicons?domain=whatsapp.com
+// @author       @JaviertINC
+// @match        *://web.whatsapp.com/*
+// @include      https://web.whatsapp.com/JaviertINC
+// @updateURL    https://javiertinc.github.io/userscripts/scripts/whatsapp.darkdefault.user.js
+// @grant        GM_addStyle
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.querySelector('body').classList.add('dark');
+
+})();
