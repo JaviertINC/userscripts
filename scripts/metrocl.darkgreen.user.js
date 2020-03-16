ce793034550a16f3267604cb694783b147f41b32
@JaviertINC <luis.cortes.jaar@gmail.com>
2020-03-16 16:57:33 -0300

    Update metrocl.darkgreen.user.js

--- scripts/metrocl.darkgreen.user.js
+++ scripts/metrocl.darkgreen.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Metro.cl | DarkGreen
-// @version      0.3
+// @version      0.5
 // @icon         https://www.google.com/s2/favicons?domain=metro.cl
 // @author       @JaviertINC
 // @match        https://metro.cl/
@@ -23,7 +23,7 @@
 		"a.fColorRojo{ position: fixed; top: 20px; left: 50px; padding: 20px!important; background-color: #4c4c4c!important; color: #4caf50!important; text-decoration: underline!important; border: 2px solid #4caf50!important; border-radius: 5px!important; }",
 		".oi.oi-x{ display: none; }",
 		"a span{ color: #f1f1f1!important; }",
-		".estadoL2 > li:nth-child(14){ background: url(\"../images/estadoL5.png\") left top no-repeat transparent!important; }",
+		//".estadoL2 > li:nth-child(14){ background: url(\"../images/estadoL5.png\") left top no-repeat transparent!important; }",
 
 	].join("\n");
 
