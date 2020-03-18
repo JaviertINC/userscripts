37973b91c25d0c9951f72e1dbeefdd9a44947f48
@JaviertINC <luis.cortes.jaar@gmail.com>
2020-03-18 20:08:29 -0300

    Update ytmp3cc.dark.user.js

--- scripts/ytmp3cc.dark.user.js
+++ scripts/ytmp3cc.dark.user.js
@@ -1,1 +1,32 @@
+// ==UserScript==
+// @name          YTmp3.cc | Dark Default
+// @version       0.1
+// @icon          https://www.google.com/s2/favicons?domain=ytmp3.cc
+// @author        @JaviertINC
+// @include       *://ytmp3.cc/*
+// @updateURL     https://javiertinc.github.io/userscripts/scripts/ytmp3cc.dark.user.js
+// @run-at        document-end
+// @grant         GM_addStyle
+// ==/UserScript==
 
+(function() {
+    'use strict';
+
+        var t = "d"
+        switch ($("link").attr("href", "/css/a/" + t + ".css?_=" + (new Date).getTime()), $("#logo").attr("src", "images/" + t + ".png"), t) {
+            case "d":
+                $(this).attr("href", "l").text("Theme [Light]");
+                break;
+            case "l":
+                $(this).attr("href", "d").text("Theme [Dark]")
+        }
+        return $.ajax({
+            url: "t.php",
+            async: !1,
+            cache: !1,
+            data: {
+                t: t
+            },
+            type: "POST"
+        })
+})();
