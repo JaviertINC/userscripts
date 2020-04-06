04b1773271f48f52ea814539dc993680d27be136
@JaviertINC <luis.cortes.jaar@gmail.com>
2020-04-06 10:52:41 -0400

    Update youtube.darkgreen.user.js

--- scripts/youtube.darkgreen.user.js
+++ scripts/youtube.darkgreen.user.js
@@ -1,1 +1,15 @@
+// ==UserScript==
+// @name          YouTube | DarkGreen
+// @description	  Una versión oscura, ayuda a cuidar tu vista :´3
+// @author        @JaviertINC
+// @version       0.1
+// @match       *://www.youtube.com/*
+// @icon          https://www.google.com/s2/favicons?domain=youtube.com
+// @updateURL     https://javiertinc.github.io/userscripts/scripts/youtube.darkgreen.user.js
+// @run-at        document-start
+// @grant         GM_addStyle
+// ==/UserScript==
+(function() {
 
+
+})();
