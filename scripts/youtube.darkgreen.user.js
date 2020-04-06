b7cdc74f903eeeee65bdd3e776be04e346b88b8b
@JaviertINC <luis.cortes.jaar@gmail.com>
2020-04-06 10:53:37 -0400

    Update youtube.darkgreen.user.js

--- scripts/youtube.darkgreen.user.js
+++ scripts/youtube.darkgreen.user.js
@@ -2,8 +2,8 @@
 // @name          YouTube | DarkGreen
 // @description	  Una versión oscura, ayuda a cuidar tu vista :´3
 // @author        @JaviertINC
-// @version       0.2
-// @match       *://www.youtube.com/*
+// @version       0.1
+// @match         *://www.youtube.com/*
 // @icon          https://www.google.com/s2/favicons?domain=youtube.com
 // @updateURL     https://javiertinc.github.io/userscripts/scripts/youtube.darkgreen.user.js
 // @run-at        document-start
@@ -12,6 +12,7 @@
 (function() {
 
     var main_color = "#4caf50";
+    var white_color = "#f1f1f1";
 
 	var css = [
         //Fill
@@ -23,6 +24,26 @@
         ".yt-icon-container.yt-icon",
 
         "{ fill: "+ main_color +"!important; }",
+
+
+        //Stroke
+        "yt-icon,",
+        ".yt-icon-container.yt-icon",
+
+        "{ stroke: unset!important; }",
+
+
+        //Backkground
+        ".ytp-swatch-background-color,",
+        ".ytp-settings-button.ytp-hd-quality-badge::after,",
+        "paper-button.ytd-subscribe-button-renderer,",
+        "ytd-sentiment-bar-renderer[activated_] #like-bar.ytd-sentiment-bar-renderer,",
+        "paper-toggle-button[checked]:not([disabled]) .toggle-button.paper-toggle-button,",
+        ".ytp-chrome-controls .ytp-button[aria-pressed]::after,",
+        "#progress.yt-page-navigation-progress,",
+        "#progress.ytd-thumbnail-overlay-resume-playback-renderer",
+
+        "{ background-color: "+ main_color +"!important; }",
 
 	].join("\n");
 	GM_addStyle(css);
