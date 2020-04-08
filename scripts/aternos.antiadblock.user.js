2bc99360e6ff59ad4c09b0f3958f771f25e94c71
@JaviertINC <luis.cortes.jaar@gmail.com>
2020-04-07 20:29:27 -0400

    Update aternos.antiadblock.user.js

--- scripts/aternos.antiadblock.user.js
+++ scripts/aternos.antiadblock.user.js
@@ -2,11 +2,11 @@
 // @name          Aternos | AntiAdblock
 // @description   Elimina el antiadblock sin dañar la usabilidad del sitio :D (Prefiere ver la publicidad, son servidores gratis, es su forma de ingresos)
 // @author        @JaviertINC
-// @version       0.1
+// @version       0.2
 // @icon          https://www.google.com/s2/favicons?domain=aternos.org
 // @updateURL     https://javiertinc.github.io/aternos-antiadblock/aternos.antiadblock.user.js
 // @match         *://aternos.org/*
-// @run-at        document-start
+// @run-at        document-end
 // @grant         GM_addStyle
 // ==/UserScript==
 
@@ -18,8 +18,35 @@
         ".main{ height: 100vh!important; }",
         ".navigation{ height: 100vh!important; }",
         "#VIWzovhkdzsrlHBImsoCljRcaXsoSwvYqreKNGDNPdT{ display: none!important; }",
+        ".header{ display: unset!important; height: 100px!important; }",
 
 	].join("\n");
-	GM_addStyle(css);
+    GM_addStyle(css);
+
+    function re_start(){
+        $('.body, .header').each(function() {
+            this.style.setProperty('display', '');
+            this.style.setProperty('height', '');
+        });
+        $('#VIWzovhkdzsrlHBImsoCljRcaXsoSwvYqreKNGDNPdT')[0].style.display = 'none';
+        $('.XcvLfpUfgbNTIbChEtgXdxUTTsyODyanOBWl').html("");
+        $('#start').each(function() {
+            this._ready = true;
+        });
+    }
+
+			re_start();
+
+		setTimeout(() => {
+			re_start();
+		}, 500);
+
+		setTimeout(() => {
+			re_start();
+		}, 1000);
+
+		setTimeout(() => {
+			re_start();
+		}, 1500);
 
 })();
