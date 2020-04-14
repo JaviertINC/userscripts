d448ba85d967f088f9ef35b3292a18257912c14c
@JaviertINC <luis.cortes.jaar@gmail.com>
2020-04-14 19:46:15 -0400

    Update netflix.darkgreen.user.js

--- scripts/netflix.darkgreen.user.js
+++ scripts/netflix.darkgreen.user.js
@@ -1,1 +1,45 @@
+// ==UserScript==
+// @name          Netflix | DarkGreen
+// @description   Una versión oscura, cuida tus ojos :D
+// @author        @JaviertINC
+// @version       0.6
+// @icon          https://www.google.com/s2/favicons?domain=netflix.com
+// @updateURL     https://javiertinc.github.io/userscripts/scripts/netflix.darkgreen.user.js
+// @match         *://www.netflix.com/*
+// @run-at        document-start
+// @grant         GM_addStyle
+// ==/UserScript==
 
+(function() {
+    'use strict';
+
+    var main_color = "#4caf50";
+    var bg_color = "#4c4c4c";
+    var bg2_color = "#242424";
+    var title_color = "#fff";
+    var subtitle = "#f1f1f1";
+    var bg_input = "#666";
+
+	var css = [
+		".pinning-header .main-header .logo,#hd .logo,.episodeLockup .episodePlay:hover .play, .episodeLockup .episodePlay:focus .play,.account-section-link,.profile-hub h3,.profile-hub .profile-change{ color:  "+ main_color +"!important; }",
+        ".progress-completed,.jawBoneContainer .menu li.current span{ background-color: "+ main_color +"!important; }",
+        "h1{ color: "+ title_color +"!important; }",
+        ".nf-flat-button-primary,.nf-flat-button,.nf-svg-button-wrapper .nf-svg-button.simpleround,.nfDropDown.theme-lakira .label{ background-color: "+ bg_color +"!important; border-color: "+ main_color +"!important; color: "+ title_color +"!important;}",
+        ".bob-card-adult .bob-play-hitzone:hover ~ .bob-overview-wrapper .svg-icon-play-with-ring,.bob-card-adult .bob-play-hitzone:hover ~ .bob-jawbone-chevron{ color: "+ main_color +"!important; }",
+        ".svg-icon.top-10-rank{ fill: "+ bg_color +"!important; }",
+        "path[fill=\"#231f20\"],circle[fill=\"#231f20\"]{ fill: "+ main_color +"!important; }",
+        "path[stroke=\"#595959\"]{ stroke: "+ main_color +"!important; }",
+        ".member-header{ background-color: "+ bg_color +"!important; border-bottom: 1px solid "+ main_color +"!important; }",
+        ".referral-page__sharing,.referral-page-steps__section{ background-color: "+ bg_color +"!important; }",
+        "html, body{ background-color: "+ bg2_color +"!important; }",
+        ".responsive-account-container,.profile-hub h4{ color: "+ subtitle +"!important; }",
+        ".referralCode__input{ background-color: "+ bg_input +"!important; }",
+        ".footer-divider{ border-color: "+ main_color +"!important; }",
+        ".site-footer-wrapper{ background-color: "+ bg_color +"!important; color: "+ subtitle +"!important; }",
+        ".footer-top-a{ color: "+ subtitle +"!important; }",
+
+
+	].join("\n");
+	GM_addStyle(css);
+
+})();
