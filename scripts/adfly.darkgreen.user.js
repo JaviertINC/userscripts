15c5ea2677ebd2746a4eb23d29321936e05fc58f
@JaviertINC <luis.cortes.jaar@gmail.com>
2020-04-15 22:33:47 -0400

    Update adfly.darkgreen.user.js

--- scripts/adfly.darkgreen.user.js
+++ scripts/adfly.darkgreen.user.js
@@ -2,7 +2,7 @@
 // @name          Adf.ly | DarkGreen
 // @description   Una versión oscura, cuida tus ojos :D
 // @author        @JaviertINC
-// @version       0.5
+// @version       0.6
 // @icon          https://www.google.com/s2/favicons?domain=adf.ly
 // @updateURL     https://javiertinc.github.io/userscripts/scripts/adfly.darkgreen.user.js
 // @match         *://adf.ly/*
@@ -32,15 +32,15 @@
         "section.page > .container{ background-color: "+ bg_color +"!important; border-color: "+ main_color +"!important; }",
         ".page h3{ color: "+ title_color +"!important; }",
         "section.dashboard section.announcements article .wrapper aside{ background-image: unset!important; background-color: "+ main_color +"!important; }",
-        "section.dashboard section.announcements article .wrapper section,body, button, input, select, textarea,summary-views_copy{ color: "+ subtitle +"!important; }",
+        "section.dashboard section.announcements article .wrapper section,body, button, input, select, textarea,summary-views_copy,.page table tbody{ color: "+ subtitle +"!important; }",
         "a:link, a:visited, a:active,#top_bar .language_switcher h5,section.page > .container .highlight ul.earnings h4{ color: "+ main_color +"!important; }",
         "#top_bar .accountType a.selected{ background: unset!important; border-top: 2px solid "+ main_color +"!important; }",
-        "#header nav li a,section.page > .container .highlight ul.earnings h4{ text-shadow: unset!important; }",
+        "#header nav li a,section.page > .container .highlight ul.earnings h4,.page h4{ text-shadow: unset!important; }",
         "section.page > .container .highlight{ border-bottom-color: "+ main_color +"!important; background-color: "+ bg_color +"!important;}",
         "section.page > .container section.bar,section.page > .container .highlight ul.earnings li,div.latest-news aside,section.dashboard .highlight nav.sort ul.time > li a{ box-shadow: unset!important; background-image: unset!important; }",
         "section.page > .container .highlight ul.earnings.totals{ background: unset!important; background-color: "+ bg2_color +"!important; }",
         "div.latest-news aside{ background-color: "+ main_color +"!important; }",
-        "section.dashboard .highlight nav.sort ul.time > li a{}",
+        ".page table tbody tr:nth-child(2n){ background-color: "+ bg3_color +"!important; }",
 	].join("\n");
 	GM_addStyle(css);
 
