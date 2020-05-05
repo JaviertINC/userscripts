a6084c561c9106646273e11181a9331f469eabe4
@JaviertINC <luis.cortes.jaar@gmail.com>
2020-05-05 13:51:41 -0400

    Update aternos.darkgreen.user.js

--- scripts/aternos.darkgreen.user.js
+++ scripts/aternos.darkgreen.user.js
@@ -2,7 +2,7 @@
 // @name          Aternos | DarkGreen
 // @description   Una versión oscura, ayuda a cuidar tus ojos :D
 // @author        @JaviertINC
-// @version       0.5
+// @version       0.6
 // @icon          https://www.google.com/s2/favicons?domain=aternos.org
 // @updateURL     https://javiertinc.github.io/aternos-antiadblock/aternos.darkgreen.user.js
 // @match         *://aternos.org/*
@@ -19,7 +19,7 @@
         ".navigation{ background-color: #4c4c4c!important; border-right: 1px solid #4caf50!important; }",
         ".navigation .item{ border-top-color: #4caf50!important; border-left-color: #333!important; border-bottom-color: #4caf50!important; }",
         ".navigation .item:last-of-type{ border-bottom-color: #4caf50!important; }",
-        ".server-ip,.btn.btn-huge.btn-green,.btn.btn-blue.btn-small.btn-clickme,.btn.btn-white.btn-small,.friend-access-small-button.friend-access-page.btn.btn-white,.btn.btn-white,.btn.btn-green{ background-color: #4c4c4c!important; border: 1px solid #4caf50!important; }",
+        ".server-ip,.btn.btn-huge.btn-green,.btn.btn-blue.btn-small.btn-clickme,.btn.btn-white.btn-small,.friend-access-small-button.friend-access-page.btn.btn-white,.btn.btn-white,.btn.btn-green,.mclogs-share.btn.btn-blue.btn-large.btn-no-margin{ background-color: #4c4c4c!important; border: 1px solid #4caf50!important; }",
         ".server-status{ border: 1px solid #4caf50!important; }",
         ".btn.btn-blue.btn-notext-mobile,.btn.btn-green.btn-small.btn-no-margin{ background-color: #666!important; border: 1px solid #4caf50!important; }",
         ".server-info-box-title{ background-color: #4c4c4c!important; border-top: 1px solid #4caf50!important; border-left: 1px solid #4caf50!important; border-right: 1px solid #4caf50!important; }",
@@ -41,6 +41,9 @@
         ".options-other{ margin: 0!important; border-radius: 3px!important; }",
         ".servericon{ border-color: #4caf50!important; }",
         ".account-setting,.account-setting{ background-color: #4c4c4c!important; border: 1px solid #4caf50!important; }",
+        ".line-number{ color: #ccc!important; }",
+        ".level-INFO .level-prefix, .level-NOTICE .level-prefix, .level-DEBUG .level-prefix, .level-TRACE .level-prefix{ color: #4caf50!important; }",
+        ".log{ background-color: #000!important; color: #f1f1f1!important; }",
 
 	].join("\n");
     GM_addStyle(css);
