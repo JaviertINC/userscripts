c2962a4390fb8be75130de66563693885392ea27
JaviertINC <github@javiertinc.cl>
2020-06-25 09:08:28 -0400

    Index Of | DarkGreen v0.9

--- scripts/indexof.darkgreen.user.js
+++ scripts/indexof.darkgreen.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Index Of
-// @description     Index Of Modificado :3
-// @version         0.8
+// @description     Index Of Modificado
+// @version         0.9
 // @author          @JaviertINC
 // @match           *://*/*
 // @exclude         *://github.com/*
@@ -28,35 +28,30 @@
     var titley = "Index of /";
     var titlez = ".js";
     function addGlobalStyle(css) {
-                var head, style;
-                head = document.getElementsByTagName('head')[0];
-                if (!head) { return; }
-                style = document.createElement('style');
-                style.type = 'text/css';
-                style.innerHTML = css;
-                head.appendChild(style);
+        var head, style;
+        head = document.getElementsByTagName('head')[0];
+        if (!head) { return; }
+        style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = css;
+        head.appendChild(style);
     }
     function Jmay(string){
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
     function Jico(){
         var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-        link.type = 'image/png';
+        link.type = 'image/x-icon';
         link.rel = 'shortcut icon';
-        link.href = '#';
-        document.getElementsByTagName('head')[0].appendChild(link);link.type = 'image/png';
+        link.href = '/favicon.ico';
+        document.getElementsByTagName('head')[0].appendChild(link);
     }
     if(titleb == titley) {
         document.title += " | IndexOf";
         addGlobalStyle('body { color: white; background-color: black; padding: 15px 30px 0 30px;} a{color: #46d11f;} a:hover{color: #4caf30;} a:visited{color: #4caf30;} li:before{content: "⇝";} li:last-child {border: none;} ul{list-style-type: none;}');
         Jico();
     }
-    if(titlef == titlez){
-        document.title = titlex + " | IndexOf";
-        addGlobalStyle('body { color: white; background-color: black; padding: 15px 30px 0 30px;} pre{color: #46d11f;}');
-        Jico();
-    }
-    if(titlej == titlew){
+    if(titlef == titlez || titlej == titlew){
         document.title = titlex + " | IndexOf";
         addGlobalStyle('body { color: white; background-color: black; padding: 15px 30px 0 30px;} pre{color: #46d11f;}');
         Jico();
