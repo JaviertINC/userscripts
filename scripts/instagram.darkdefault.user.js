63535ae5e8c13d1cab87cb241d4254e99ce5f099
JaviertINC <github@javiertinc.cl>
2020-07-01 12:32:58 -0400

    Instagram | DarkDefault v0.1

---
+++ scripts/instagram.darkdefault.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Instagram | DarkDefault
+// @namespace    https://javiertinc.cl/userscripts
+// @version      0.1
+// @description  Una versión oscura, ayuda a cuidar tus ojos :D
+// @homepage     https://javiertinc.cl/userscripts/script/instagram.darkdefault
+// @author       @JaviertINC
+// @match        *://www.instagram.com/*
+// @include      *://instagram.com/*
+// @updateURL    https://javiertinc.github.io/userscripts/scripts/instagram.darkdefault.user.js
+// @icon         https://www.google.com/s2/favicons?domain=instagram.com
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.querySelector('body').classList.add('ekjoN');
+
+})();
